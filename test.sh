170d5c50fa78afd461e95ffdd47a1615100902e0
mvanhulsentop <ec2-user@ip-172-16-32-110.eu-west-1.compute.internal>
2014-01-15 15:02:53 -0500

    Updated web listen port

--- test.sh
+++ test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 export OPENSHIFT_JBOSSAS6_DIR=../testrun
+export OPENSHIFT_JBOSSAS6_HTTP_PORT=8070
+export OPENSHIFT_JBOSSAS6_IP=127.0.0.1
 export OPENSHIFT_CARTRIDGE_SDK_BASH=/home/ec2-user/origin-server-master/node/misc/usr/lib/cartridge_sdk/bash/sdk
 
 echo Using $OPENSHIFT_JBOSSAS6_DIR as test dir
+
+echo Wait for shutdown
+killall java
+sleep 5
+
 rm -rf $OPENSHIFT_JBOSSAS6_DIR
 mkdir $OPENSHIFT_JBOSSAS6_DIR
 
